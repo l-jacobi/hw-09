fb86ff710ea31134d7baa2e2fdf1de52876ecbf3
RGuglin <78051103+RGuglin@users.noreply.github.com>
2021-11-22 02:42:58 -0800

    Add files via upload

--- cities.cc
+++ cities.cc
@@ -1,13 +1,47 @@
-#include "cities.hh"
 #include <iostream>
+#include <random>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include "cities.hh"
 
-std::istream& operator>> (std::istream& is, Cities& cities){
-	//getine, go until it reaches space? then until it ends?
-	int x, y;
-	while(!is.eof()){
-		is >> x >> y;
-		cities.map_.push_back(Cities::coord_t(x, y));
-	}
-	cities.map_.pop_back();	//remove weird last element idk what to do about
-	return is;
+using namespace std;
+
+Cities::permutation_t
+random_permutation(unsigned len)
+{
+  assert(int(len) > 0);
+  Cities::permutation_t out;
+  int end = len-1;
+  unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+  //makes sure each prog call returns a different vector, even within the same comp
+  //is this necessary? seems cumbersome
+  static default_random_engine generator (seed);
+  uniform_int_distribution<unsigned int> distribution(0, end);
+  while(out.size() != len){
+    auto num = distribution(generator);
+    if(count(out.begin(), out.end(), num) == 0){
+      out.push_back(num);
+    }
+    else{ auto num = distribution(generator); }
+  }
+  return out;
+}
+
+void test(int len)
+{
+  auto vec = random_permutation(len);
+  for(auto i=0; i < vec.size()-1; ++i){
+    cout<<vec[i]<<",  ";
+  }
+  cout<<vec[vec.size()-1]<<endl;
+}
+
+int main()
+{
+  test(1);
+  test(5);
+  test(10);
+  test(20);
+  return 0;
 }
