43de0f6a576e071706ef17208db718cc5e95b973
Rebecca Guglin <rebeccaguglin@c125h236.wless.reed.edu>
2021-11-22 04:17:04 -0800

    poop

--- cities.hh
+++ cities.hh
@@ -17,13 +17,13 @@ class Cities {
   // into the current city ordering in some container.
   using permutation_t = std::vector<unsigned int>;
 
+  permutation_t random_permutation(unsigned len);
+
   // Given a permutation, return a new Cities object where the order of the
   // cities reflects the original order of this class after reordering with
   // the given ordering. So for example, the ordering { 1, 0 } simply swaps
   // the first two elements (coordinates) in the new Cities object.
   Cities reorder(const permutation_t& ordering) const;
-
-  permutation_t random_permutation(unsigned len);
 
   // For a given permutation of the cities in this object,
   // compute how long (distance) it would take to traverse all the cities in the
