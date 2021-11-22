6766762ca8419cb115b61a8d10f2a441447afed3
Rebecca Guglin <rebeccaguglin@c125h236.wless.reed.edu>
2021-11-22 03:39:02 -0800

    cities

--- cities.hh
+++ cities.hh
@@ -4,38 +4,31 @@
 
 #pragma once
 
-#include <iostream>
 #include <vector>
 
 
 // Representation of an ordering of cities
 class Cities {
-	public:
-		// A pair of integral coordinates for each city
-		using coord_t = std::pair<int, int>;
+ public:
+  // A pair of integral coordinates for each city
+  using coord_t = std::pair<int, int>;
 
-		// An ordering of cities. Each value represents a unique index
-		// into the current city ordering in some container.
-		using permutation_t = std::vector<unsigned int>;
+  // An ordering of cities. Each value represents a unique index
+  // into the current city ordering in some container.
+  using permutation_t = std::vector<unsigned int>;
 
-		// Given a permutation, return a new Cities object where the order of the
-		// cities reflects the original order of this class after reordering with
-		// the given ordering. So for example, the ordering { 1, 0 } simply swaps
-		// the first two elements (coordinates) in the new Cities object.
-		Cities reorder(const permutation_t& ordering) const;
+  // Given a permutation, return a new Cities object where the order of the
+  // cities reflects the original order of this class after reordering with
+  // the given ordering. So for example, the ordering { 1, 0 } simply swaps
+  // the first two elements (coordinates) in the new Cities object.
+  Cities reorder(const permutation_t& ordering) const;
 
-		// For a given permutation of the cities in this object,
-		// compute how long (distance) it would take to traverse all the cities in
-		// the order of the permutation, and then returning to the first city.
-		// The distance between any two cities is computed as the Euclidean 
-		// distance on a plane between their coordinates.
-		double total_path_distance(const permutation_t& ordering) const;
+  permutation_t random_permutation(unsigned len);
 
-////debug block
-	//	friend void debug_print_city(Cities cities);
-
-	private:
-		std::vector<coord_t> map_;
-	friend std::istream& operator>> (std::istream& is, Cities& cities);
-	friend std::ostream& operator<< (std::ostream& os, Cities& cities);
+  // For a given permutation of the cities in this object,
+  // compute how long (distance) it would take to traverse all the cities in the
+  // order of the permutation, and then returning to the first city.
+  // The distance between any two cities is computed as the Euclidean
+  // distance on a plane between their coordinates.
+  double total_path_distance(const permutation_t& ordering) const;
 };
--- el_cities.cc
+++ el_cities.cc
@@ -1,5 +1,9 @@
 #include "cities.hh"
 #include <iostream>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
 using coord_t = Cities::coord_t;
 
 std::istream& operator>> (std::istream& is, Cities& cities){
@@ -18,4 +22,27 @@ std::ostream& operator<< (std::ostream& os, Cities& cities){
 		os << coord.first << " " << coord.second << std::endl;
 	}
 	return os;
+}
+
+using namespace std;
+
+Cities::permutation_t
+Cities::random_permutation(unsigned len)
+{
+  assert(int(len) > 0);
+  Cities::permutation_t out;
+  int end = len-1;
+  unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+  //makes sure each prog call returns a different vector, even within the same comp
+  //is this necessary? seems cumbersome
+  static default_random_engine generator (seed);
+  uniform_int_distribution<unsigned int> distribution(0, end);
+  while(out.size() != len){
+    auto num = distribution(generator);
+    if(count(out.begin(), out.end(), num) == 0){
+      out.push_back(num);
+    }
+    else{ auto num = distribution(generator); }
+  }
+  return out;
 }
